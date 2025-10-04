1d7882cc536ab3f0a8cff6eb2fa7ac9e3fb25abe
Darragh Lydon <D.Lydon@qub.ac.uk>
2025-10-04 07:56:25 +0100

    Adding if checks to gameplay, and some strings for our output texts

--- Program.cs
+++ Program.cs
@@ -27,6 +27,27 @@ int batteryCapacity = randomIntegerGenerator.Next(1, numberOfGhosts);
 //Everyone is a little bit crazy, but we'll assume you've got some level of confidence so your sanity won't start below 3. 
 int playerSanity = randomIntegerGenerator.Next(3,7);
 int roomNumber = 1;
+//Every ghost has a level, which we must overcome through a random dice roll to defeat
+int ghostLevel = randomIntegerGenerator.Next(1, 5);
+//this value will be used everytime we face a ghost
+int scareAwayValue;
+
+
+//To make our code a bit more readable, and to allow us to change these messages later we can define them as a string
+string ghostInRoom = "There's a ghost in this room! Spppooookkkkyyyy!!!";
+string noGhostInRoom = "There are no ghosts here!";
+string willYouScareGhost = "Do you want to try to scare it?";
+string attemptToScareGhost = "You shakily point your torch at the ghost.";
+string scareAwayFailed = "Your torch wobbles over the ghost, but it has no effect! You run in terror!";
+string scareAwaySuccess = "Your torch beam smashes into the ghost, making it disappear! You stride into the next room confidently.";
+string runAwayText = "You shriek and run from the ghost into the next room, slamming the door behind you.";
+//String formats allow us to make code a bit more readable. The number in the curly brackets should be replaced by the variable at the end
+string batteryCapacityText = string.Format("You have a battery capacity of {0}", batteryCapacity);
+string batteryEmptyText = "You have no battery left!";
+string gameSuccess = "You scared away all the ghosts! A clear light shines through Spooky House, with a new day of hope!";
+string gameFailure =
+    " You have gone totally, utterly mad. You are doomed to wander spooky house until you join the ghosts.";
+
 
 
 //For the game itself, if you see a ghost in a room and choose not to scare it away before you leave, your sanity will drop by 1.
@@ -42,7 +63,7 @@ string playerName = "";
 #endregion
 
 
-//So far, all we can do is take the player's name and tell them some information about Spooky House. 
+//Take the player's name and tell them some information about Spooky House. 
 Console.WriteLine("Welcome to Spooky House!");
 Console.Write("What is your name? ");
 playerName = Console.ReadLine();
@@ -51,37 +72,77 @@ Console.WriteLine("Welcome "+ playerName+". You are feeling as sane as the numbe
 Console.WriteLine("You have a battery capacity on your torch of "+batteryCapacity);
 Console.WriteLine("Which is unfortunate, as there are "+numberOfGhosts + " spooky ghosts in the house.");
 Console.WriteLine("You are in Room " + roomNumber);
-int nextValue = randomIntegerGenerator.Next(0, 1);
-if (nextValue==1)
-{
-    Console.WriteLine("There is a ghost in this room")
-    isThereAGhostInThisRoom = true;
-}
 
-if (isThereAGhostInThisRoom)
+//If there are still ghosts in the house, check if there's one in this room
+if (numberOfGhosts > 0)
 {
-    Console.WriteLine("Do you want to scare the ghost with your torch?");
-    string answer = Console.ReadLine();
-    if (answer == 'Yes')
+//There is a 50/50 chance there's a ghost in the room
+    int ghostInRoomCheck = randomIntegerGenerator.Next(0, 1);
+    if (ghostInRoomCheck == 1)
     {
-        if (batteryCapacity > 0)
+        Console.WriteLine(ghostInRoom);
+        isThereAGhostInThisRoom = true;
+    }
+
+    if (isThereAGhostInThisRoom)
+    {
+        Console.WriteLine(batteryCapacityText);
+        Console.WriteLine(willYouScareGhost);
+        string answer = Console.ReadLine();
+        if (answer == 'Yes')
         {
-            Console.WriteLine("You scared away the ghost!");
-            numberOfGhosts = numberOfGhosts - 1;
+            if (batteryCapacity > 0)
+            {
+                Console.WriteLine(attemptToScareGhost);
+                //Find the ghost's level, and check if we can scare it away
+                ghostInRoomCheck = randomIntegerGenerator.Next(1,5);
+                scareAwayValue = randomIntegerGenerator.Next(1, 6);
+                if (scareAwayValue >= ghostInRoomCheck)
+                {
+                    Console.WriteLine(scareAwaySuccess);
+                    numberOfGhosts = numberOfGhosts - 1;
+                    playerSanity = playerSanity + 1;
+                }
+                else
+                {
+                    Console.WriteLine(scareAwayFailed);
+                    //trying to scare away a ghost and failing is worse than just running from one.
+                    playerSanity = playerSanity - 2;
+                }
+                batteryCapacity = batteryCapacity - 1;
+                
+                
+            }
+            else
+            {
+                Console.WriteLine(batteryEmptyText);
+                playerSanity = playerSanity - 1;
+            }
+
         }
         else
         {
-            Console.WriteLine("You have no battery left!");
+            Console.WriteLine(runAwayText);
             playerSanity = playerSanity - 1;
         }
-        
+
     }
     else
     {
-        Console.WriteLine("Ghosts are scurry");
-        playerSanity = playerSanity - 1; 
+        Console.WriteLine(noGhostInRoom);
     }
 
+    if (numberOfGhosts == 0)
+    {
+        Console.WriteLine(gameSuccess);
+    }
+    else if (playerSanity <= 0)
+    {
+        Console.WriteLine(gameFailure);
+    }
+        
 }
+
+
 
 
